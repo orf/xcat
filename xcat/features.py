2709d07bc4d7c9f3958fa332d84346e92249990f
Tom Forbes <tom.forbes@contextis.co.uk>
2016-12-09 12:00:26 +0000

    Add xpath 3 detection

--- xcat/features.py
+++ xcat/features.py
@@ -1,17 +1,24 @@
 from collections import namedtuple
 from typing import List
 
-from xcat.xpath.xpath_3 import available_environment_variables, unparsed_text_available
+from xcat.xpath.xpath_3 import available_environment_variables, unparsed_text_available, generate_id
 from .algorithms import ASCII_SEARCH_SPACE
 from .requester import Requester
-from .xpath import F, E
-from .xpath.xpath_1 import string_length, substring_before, function_available, boolean, string
-from .xpath.xpath_2 import string_to_codepoints, lower_case, exists, document_uri, current_date_time, doc, doc_available
+from .xpath import E
+from .xpath.xpath_1 import string_length, substring_before, string, boolean
+from .xpath.xpath_2 import string_to_codepoints, lower_case, exists, document_uri, current_date_time, doc_available
 
 Feature = namedtuple('Feature', 'name tests')
 
-
 features = [
+    Feature('xpath-2',
+            [
+                lower_case('A') == 'a'
+            ]),
+    Feature('xpath-3',
+            [
+                boolean(generate_id(E('/')))
+            ]),
     Feature('substring-search',
             [
                 string_length(substring_before(ASCII_SEARCH_SPACE, 'h')) == ASCII_SEARCH_SPACE.find('h'),
@@ -23,10 +30,6 @@ features = [
                 string_to_codepoints("test")[2] == 101,
                 string_to_codepoints("test")[3] == 115,
                 string_to_codepoints("test")[4] == 116,
-            ]),
-    Feature('xpath-2',
-            [
-                lower_case('A') == 'a'
             ]),
     Feature('environment-variables',
             [
--- xcat/xpath/xpath_3.py
+++ xcat/xpath/xpath_3.py
@@ -2,3 +2,4 @@ from . import Function
 
 available_environment_variables = Function('available-environment-variables')
 unparsed_text_available = Function('unparsed-text-available', args_count=1)
+generate_id = Function('generate-id', args_count=1)
